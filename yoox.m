7fe985cbfaf354c10d8bcf6dcbb25a54c4a2b095
twitchard <twitchard@users.noreply.github.com>
2023-09-10 07:30:33 +0000

    Progress

---
+++ yoox.m
@@ -0,0 +1,47 @@
+:- module yoox.
+:- interface.
+:- import_module list.
+:- import_module parsing_utils.
+:- type program ---> program(list(statement)).
+:- type statement --->
+      equality(identifier, value) ;
+      action(identifier, list(value)).
+:- type value --->
+      int_value(int) ;
+      identifier_value(identifier).
+:- type identifier == string.
+
+:- pred parse_program(src::in, program::out, ps::in, ps::out) is semidet.
+:- implementation.
+
+parse_program(Src, Out, !PS) :- 
+  one_or_more(parse_statement, Src, List, !PS),
+  Out = program(List).
+  
+:- pred parse_statement(src::in, statement::out, ps::in, ps::out) is semidet.
+parse_statement(Src, Out, !PS) :- 
+  if parse_equality(Src, Id, Val, !PS), punct(";", Src, _, !PS) then Out = equality(Id, Val)
+  else parse_action(Src, Id, Vals, !PS), punct(";", Src, _, !PS), Out = action(Id, Vals).
+
+:- pred parse_identifier(src::in, identifier::out, ps::in, ps::out).
+parse_identifier(Src, Out, !PS) :-
+  parsing_utils.identifier("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", Src, Out, !PS).
+
+:- pred parse_equality(src::in, identifier::out, value::out, ps::in, ps::out) is semidet.
+parse_equality(Src, A, B, !PS) :-
+  parse_identifier(Src, A, !PS),
+  punct("==", Src, _, !PS),
+  int_literal(Src, Int, !PS),
+  B = int_value(Int).
+  
+:- pred parse_action(src::in, identifier::out, list(value)::out, ps::in, ps::out) is semidet.
+parse_action(Src, Identifier, Values, !PS) :-
+  parse_identifier(Src, Identifier, !PS),
+  brackets("(", ")", comma_separated_list(parse_value), Src, Values, !PS).
+
+:- pred parse_value(src::in, value::out, ps::in, ps::out) is semidet.
+
+parse_value(Src, Out, !PS) :-
+  if parse_identifier(Src, Id, !PS)
+  then Out = identifier_value(Id)
+  else int_literal(Src, Int, !PS), Out = int_value(Int).
